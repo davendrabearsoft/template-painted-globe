5e3be653422ba43b876c7cf5c71e152aa0eac66c
davendrabearsoft <116486183+davendrabearsoft@users.noreply.github.com>
2022-12-22 11:16:20 -0600

    Update HeroText.vue

--- src/components/HeroText.vue
+++ src/components/HeroText.vue
@@ -3,23 +3,21 @@
     <h1
       class="relative mb-4 text-2xl font-black leading-tight text-gray-900 sm:text-6xl xl:mb-8"
     >
-      Move your mouse on the globe!
+      Move your mouse around!
     </h1>
     <p class="pr-0 mb-8 text-base text-gray-600 sm:text-lg xl:text-xl pl-32">
-      <span class="block">Bleep Boop</span>
-      <span>Blah Blah</span>
+      <span class="block">Bleep Boop!</span>
     </p>
     <a
       href="mailto:youremail@yourdomain.com"
       class="relative self-start inline-block w-auto px-8 py-4 mx-auto mt-0 text-base font-bold text-white bg-indigo-600 border-t border-gray-200 rounded-md shadow-xl sm:mt-1 fold-bold lg:mx-0"
-      >Let's talk</a>
-
+      >Button</a
+    >
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-
 export default defineComponent({
   name: "Hero-Text",
 });
