d1407044067bed35ba03d82c8fc3ba1e93632a5a
davendrabearsoft <116486183+davendrabearsoft@users.noreply.github.com>
2022-12-22 11:13:00 -0600

    Update HeroText.vue

--- src/components/HeroText.vue
+++ src/components/HeroText.vue
@@ -9,12 +9,11 @@
       <span class="block">Bleep Boop</span>
       <span>Blah Blah</span>
     </p>
-<!--
     <a
       href="mailto:youremail@yourdomain.com"
       class="relative self-start inline-block w-auto px-8 py-4 mx-auto mt-0 text-base font-bold text-white bg-indigo-600 border-t border-gray-200 rounded-md shadow-xl sm:mt-1 fold-bold lg:mx-0"
       >Let's talk</a>
--->
+
   </div>
 </template>
 
